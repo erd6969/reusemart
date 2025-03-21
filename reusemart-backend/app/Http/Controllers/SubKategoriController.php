8d5ed91479ccf657fd886e3672adf33d5b6c51b8
erd <220711618@students.uajy.ac.id>
2025-03-21 21:38:23 +0700

    add sub category

---
+++ reusemart-backend/app/Http/Controllers/SubKategoriController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sub_kategori;
+use Illuminate\Http\Request;
+
+class SubKategoriController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sub_kategori $sub_kategori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, sub_kategori $sub_kategori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sub_kategori $sub_kategori)
+    {
+        //
+    }
+}
---
+++ reusemart-backend/database/migrations/2025_03_21_143716_create_sub_kategoris_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_kategoris', function (Blueprint $table) {
+            $table->id('id_sub_kategori');
+            $table->unsignedBigInteger('id_kategori');
+            $table->string('nama_sub_kategori');
+            $table->timestamps();
+
+            $table->foreign('id_kategori')
+                ->references('id_kategori')
+                ->on('kategori')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_kategoris');
+    }
+};

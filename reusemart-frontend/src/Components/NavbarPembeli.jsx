faabe802223903f917b1ec3179b5e2eca1ae46c3
erd <220711618@students.uajy.ac.id>
2025-03-31 18:38:37 +0700

    update navbar
    update navbar

--- reusemart-frontend/src/Components/NavbarPembeli.jsx
+++ reusemart-frontend/src/Components/NavbarPembeli.jsx
@@ -1,37 +1,44 @@
-import { useNavigate } from 'react-router-dom';
-import React, { useState, useRef } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import './NavbarPembeli.css';
-import profileImage from '../assets/images/Pembeli/Yuki.jpeg';
-import logoReuseMart from '../assets/images/logo-reusemart.png';
-import coin from '../assets/images/coin-icon.png';
-import { FaShoppingCart, FaChevronDown } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./NavbarPembeli.css";
+import profileImage from "../assets/images/Pembeli/Yuki.jpeg";
+import logoReuseMart from "../assets/images/logo-reusemart.png";
+import coin from "../assets/images/coin-icon.png";
+import { FaShoppingCart, FaChevronDown, FaBars, FaTimes } from "react-icons/fa";
 
 const TopNavbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const navigate = useNavigate();
     const dropdownRef = useRef(null);
 
     return (
         <Navbar className="navBar">
             <Container fluid>
-                <img src={logoReuseMart} alt="logo" className="logoReuseMart" onClick={() => navigate('/')} />
-                <Navbar.Brand className="navTitle" onClick={() => navigate('/')}>ReuseMart</Navbar.Brand>
-                
-                <Nav className="me-auto navContent justify-content-center">
+                <div className="navLeft" onClick={() => navigate("/")}>
+                    <img src={logoReuseMart} alt="logo" className="logoReuseMart" />
+                    <Navbar.Brand className="navTitle">ReuseMart</Navbar.Brand>
+                </div>
+
+                <div className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    {isMenuOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
+                <Nav className={`navContent ${isMenuOpen ? "open" : ""}`}>
                     <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
                     <Nav.Link onClick={() => navigate("/shop")}>Shop</Nav.Link>
                     <Nav.Link onClick={() => navigate("/help")}>Help</Nav.Link>
                 </Nav>
 
                 <FaShoppingCart className="cartIcon" />
-                
-                <div 
+
+                <div
                     className="profileContainer"
                     ref={dropdownRef}
-                    onMouseEnter={() => setIsOpen(true)} // Muncul saat hover
-                    onMouseLeave={() => setIsOpen(false)} // Hilang saat keluar hover
-                    onClick={() => setIsOpen((prev) => !prev)} // Bisa juga dibuka dengan klik
+                    onMouseEnter={() => setIsDropdownOpen(true)}
+                    onMouseLeave={() => setIsDropdownOpen(false)}
+                    onClick={() => setIsDropdownOpen((prev) => !prev)}
                 >
                     <div className="profileSection">
                         <img src={profileImage} alt="profile" />
@@ -39,34 +46,42 @@ const TopNavbar = () => {
                         <FaChevronDown className="chevronIcon" />
                     </div>
 
-                    {isOpen && (
-                        <div className="dropdownMenu">
-                            <div className="dropdownContent">
-                                <div className="pointsSection">
-                                    <img src={coin} alt="coin icon" />
-                                    <div className="pointsText">
-                                        <span>Loyalty Points</span>
-                                        <strong>1000 Points</strong>
-                                    </div>
-                                </div>
-                                <div className="menuSection">
-                                    <ul>
-                                        <li><Nav.Link  onClick={() => navigate("/pembeli/profile")}>My Profile</Nav.Link></li>
-                                        <li><Nav.Link  onClick={() => navigate("/pembeli/alamat")}>My Address</Nav.Link></li>
-                                        <li><Nav.Link  onClick={() => navigate("/pembeli/purchase")}>Purchase</Nav.Link></li>
-                                    </ul>
-                                    <hr />
-                                    <div className="logout">
-                                        Log Out <span>➡</span>
-                                    </div>
+                    <div className={`dropdownMenu ${isDropdownOpen ? "show" : ""}`}>
+                        <div className="dropdownContent">
+                            <div className="pointsSection">
+                                <img src={coin} alt="coin icon" />
+                                <div className="pointsText">
+                                    <span>Loyalty Points</span>
+                                    <strong>1000 Points</strong>
                                 </div>
                             </div>
+                            <div className="menuSection">
+                                <ul>
+                                    <li>
+                                        <Nav.Link onClick={() => navigate("/pembeli/profile")}>
+                                            My Profile
+                                        </Nav.Link>
+                                    </li>
+                                    <li>
+                                        <Nav.Link onClick={() => navigate("/pembeli/alamat")}>
+                                            My Address
+                                        </Nav.Link>
+                                    </li>
+                                    <li>
+                                        <Nav.Link onClick={() => navigate("/pembeli/purchase")}>
+                                            Purchase
+                                        </Nav.Link>
+                                    </li>
+                                </ul>
+                                <hr />
+                                <div className="logout">Log Out ➡</div>
+                            </div>
                         </div>
-                    )}
+                    </div>
                 </div>
             </Container>
         </Navbar>
     );
-}
+};
 
 export default TopNavbar;

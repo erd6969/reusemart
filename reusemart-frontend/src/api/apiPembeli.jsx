c8686eb7b4c4d12000eaf096154c8cb2e062c549
Febry <febrysimahan02@gmail.com>
2025-05-02 02:10:04 +0700

    logika profile

--- reusemart-frontend/src/api/apiPembeli.jsx
+++ reusemart-frontend/src/api/apiPembeli.jsx
@@ -1,5 +1,9 @@
 import useAxios from ".";
 
+const getToken = () => {
+    return sessionStorage.getItem("token");
+};
+
 export const RegisterPembeli = async (data) => {
     try {
         const response = await useAxios.post("/pembeli/register", data, {
@@ -25,4 +29,19 @@ export const RegisterPembeli = async (data) => {
             message: error.response?.data?.message || "Terjadi kesalahan saat registrasi.",
         };
     }
+};
+
+export const GetProfile = async () => {
+    try {
+        const response = await useAxios.get("/pembeli/show-profile", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getToken()}`,
+          },
+        });
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching profile", error);
+        throw error?.response?.data || error;
+      }
 };

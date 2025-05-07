d68d32686807c38e3f1faa0f458d0573ce8bbbbe
Fythen <220711697@students.uajy.ac.id>
2025-05-07 15:47:54 +0700

    test

--- reusemart-frontend/src/api/apiRequestDonasi.jsx
+++ reusemart-frontend/src/api/apiRequestDonasi.jsx
@@ -42,3 +42,60 @@ export const SearchRequestDonasi = async (search_request_donasi) => {
         throw error;
     }
 }
+
+export const ShowRequestDonasiByIdBarang = async (id_barang) => {
+    try {
+        const token = getToken();
+
+        const response = await useAxios.get(`/request_donasi/show-by-id/${id_barang}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+        console.log("Show Request Donasi by ID Barang response:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching request donasi by ID barang:", error);
+        throw error;
+    }
+}
+
+export const ShowWaitingRequestDonasi = async () => {
+    try {
+        const token = getToken();
+
+        const response = await useAxios.get(`/request_donasi/show-waiting-request`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+        console.log("Show Waiting Request Donasi response:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching waiting request donasi:", error);
+        throw error;
+    }
+}
+
+export const AcceptRequestDonasi = async (data) => {
+    try {
+        const token = getToken();
+
+        const response = await useAxios.post(`/request_donasi/acceptRequest`, data, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+        console.log("Accept Request Donasi response:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error accepting request donasi:", error);
+        throw error;
+    }
+}

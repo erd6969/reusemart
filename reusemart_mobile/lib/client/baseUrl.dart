30ca8ec3b403bb9cbfaf476346d58bc4b3aac7fe
erd <220711618@students.uajy.ac.id>
2025-06-09 20:57:26 +0700

    update baseUrl
    update baseUrl

--- reusemart_mobile/lib/client/baseUrl.dart
+++ reusemart_mobile/lib/client/baseUrl.dart
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 // const String baseUrl = '192.168.0.109:8000'; // Agus
 const String baseUrl = '192.168.100.198:8000'; // Eric
 // const String baseUrl = '10.0.2.2:8000'; // Febry
-=======
-const String baseUrl = '192.168.0.105:8000'; // Agus
-// const String baseUrl = '10.0.2.2:8000'; // Erik
-// const String baseUrl = '192.168.0.113:8000'; // Febry
->>>>>>> main
